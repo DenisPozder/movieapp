d26f6e541af24cfb4a124c9383a39f1bef57ae53
DenisPozder <143366207+DenisPozder@users.noreply.github.com>
2024-10-29 02:52:58 +0100

    Update Axios.js

--- client/src/Redux/APIs/Axios.js
+++ client/src/Redux/APIs/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const Axios = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://realestate-beta-seven.vercel.app/api",
 })
 
 export default Axios
